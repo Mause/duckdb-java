31c699e700637e315cd10c7ba8635512f11f11ac
Elliana May <me@mause.me>
2024-06-15 23:05:57 +0800

    tyoe information access

--- src/main/java/org/duckdb/DuckDBLibrary.java
+++ src/main/java/org/duckdb/DuckDBLibrary.java
@@ -55,6 +55,85 @@ public class DuckDBLibrary implements InfoMapper {
         public native Value GetValue(int row_idx, int col_idx);
     }
 
+    public enum LogicalTypeId {
+        INVALID(0),
+        SQLNULL(1), /* NULL type, used for constant NULL */
+        UNKNOWN(2), /* unknown type, used for parameter expressions */
+        ANY(3),     /* ANY type, used for functions that accept any type as parameter */
+        USER(4),    /* A User Defined Type (e.g., ENUMs before the binder) */
+        BOOLEAN(10),
+        TINYINT(11),
+        SMALLINT(12),
+        INTEGER(13),
+        BIGINT(14),
+        DATE(15),
+        TIME(16),
+        TIMESTAMP_SEC(17),
+        TIMESTAMP_MS(18),
+        TIMESTAMP(19), //! us
+        TIMESTAMP_NS(20),
+        DECIMAL(21),
+        FLOAT(22),
+        DOUBLE(23),
+        CHAR(24),
+        VARCHAR(25),
+        BLOB(26),
+        INTERVAL(27),
+        UTINYINT(28),
+        USMALLINT(29),
+        UINTEGER(30),
+        UBIGINT(31),
+        TIMESTAMP_TZ(32),
+        TIME_TZ(34),
+        BIT(36),
+        STRING_LITERAL(37), /* string literals, used for constant strings - only exists while binding */
+        INTEGER_LITERAL(38),/* integer literals, used for constant integers - only exists while binding */
+
+        UHUGEINT(49),
+        HUGEINT(50),
+        POINTER(51),
+        VALIDITY(53),
+        UUID(54),
+
+        STRUCT(100),
+        LIST(101),
+        MAP(102),
+        TABLE(103),
+        ENUM(104),
+        AGGREGATE_STATE(105),
+        LAMBDA(106),
+        UNION(107),
+        ARRAY(108);
+
+        public final int value;
+
+        private LogicalTypeId(int v) {
+            this.value = v;
+        }
+
+        private LogicalTypeId(LogicalTypeId e) {
+            this.value = e.value;
+        }
+
+        public LogicalTypeId intern() {
+            for (LogicalTypeId e : values()) if (e.value == value) return e;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return intern().name();
+        }
+    }
+
+    public static class LogicalType extends Pointer {
+        public native LogicalTypeId id();
+
+        @Name("ToString")
+        @StdString
+        public native String toString();
+    }
+
     @Name("hugeint_t")
     public static class HugeInt extends Pointer {
         @MemberGetter
@@ -85,6 +164,10 @@ public class DuckDBLibrary implements InfoMapper {
 
         @Name("GetValue<int8_t>")
         public native int asInt8();
+
+        @Const
+        @ByVal
+        public native LogicalType type();
     }
 
     public static class DatabaseInstance extends Pointer {
